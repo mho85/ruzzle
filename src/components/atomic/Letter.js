1ce76430830f11d4d95cf3023ae3588a524af7df
Michel Ho <mho85@icloud.com>
2020-07-16 22:10:32 +0200

    Random letter rotation

--- src/components/atomic/Letter.js
+++ src/components/atomic/Letter.js
@@ -3,9 +3,23 @@ import '../../styles/Letter.scss'
 
 
 export default function Letter({ letter }) {
+
+  const underline = (character) => {
+    return character === 'M' || character === 'W' ? "underline" : "initial"
+  }
+  const randomRotate = () => {
+    const angles = [0, 90, 180, 270]
+    const randomAngle = angles[Math.floor(Math.random() * angles.length)]
+    return `rotate(${randomAngle}deg)`
+  }
+
   return (
     <div id="letter-container">
-      {letter}
+      <p
+        style={{ textDecoration: underline(letter), transform: randomRotate() }}
+      >
+        {letter}
+      </p>
     </div>
   )
 }
